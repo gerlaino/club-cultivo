4fdda663100e92eab25e5c3fc214ea93201aa2ac
= <=>
2025-10-14 13:29:19 +0000

    base controller fixed to deploy

--- backend/app/controllers/base_controller.rb
+++ backend/app/controllers/base_controller.rb
@@ -1,6 +1,5 @@
 class BaseController < ApplicationController
   before_action :authenticate_user!
-  protect_from_forgery with: :null_session
   respond_to :json
 
   rescue_from ActiveRecord::RecordNotFound do
