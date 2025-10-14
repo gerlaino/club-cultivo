315fc87860d206293ad8e67e4e7c8ae336afb8bd
= <=>
2025-10-14 13:24:12 +0000

    base controller fixed to deploy

--- backend/app/controllers/base_controller.rb
+++ backend/app/controllers/base_controller.rb
@@ -1,4 +1,4 @@
-class Api::BaseController < ApplicationController
+class BaseController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery with: :null_session
   respond_to :json
