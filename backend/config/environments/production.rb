de07a684bd7cc4490eb8b2c64117f35ca0d58a64
= <=>
2025-10-16 20:13:29 +0000

    wip

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_dispatch.cookies_same_site_protection = :none
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
