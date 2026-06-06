d4d972509324b0d94b421e15cc64fd0304206de0
= <=>
2026-06-06 13:41:07 +0000

    fixing migrations in prod

--- backend/db/migrate/20260605000002_add_pesaje_manicura_to_pesadas_plantas.rb
+++ backend/db/migrate/20260605000002_add_pesaje_manicura_to_pesadas_plantas.rb
@@ -1,7 +1,7 @@
 class AddPesajeManicuraToPesadasPlantas < ActiveRecord::Migration[7.0]
   def change
     add_reference :pesadas_plantas, :pesaje_manicura, null: true, foreign_key: { to_table: :pesajes_manicura }
-    add_index :pesadas_plantas, :pesaje_manicura_id
+    add_index :pesadas_plantas, :pesaje_manicura_id, if_not_exists: true
   end
 
   def down
